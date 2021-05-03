e3191d8f53557b8bacd2852c1308fdc1b902696f
tmddusqls <tmddusqls@naver.com>
2021-05-03 20:52:31 +0900

    세부 요소 수정

---
+++ 210503/app/src/main/java/com/example/a210503/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.a210503
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, Fragment1())
+            .commit()
+
+    }
+}
