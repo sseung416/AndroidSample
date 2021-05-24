323b646acd3a1a76dc030873f3699bf205108b15
tmddusqls <tmddusqls@naver.com>
2021-05-24 13:51:17 +0900

    ToDoList item 디자인 변경

--- ToDoList/app/src/main/java/com/example/todolist/ItemData.kt
+++
@@ -1,3 +0,0 @@
-package com.example.todolist
-
-data class ItemData(val color: Int, val title: String, val content: String)
---
+++ ToDoList/app/src/main/java/com/example/todolist/data/TaskIListData.kt
@@ -0,0 +1,3 @@
+package com.example.todolist.data
+
+data class TaskIListData(val color: Int, val title: String, val content: String)
