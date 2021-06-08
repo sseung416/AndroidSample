c9a6e5cec6592ad3e37d4e1041d9e7e1e0caabac
tmddusqls <tmddusqls@naver.com>
2021-06-08 09:10:11 +0900

    retrofit 연습

--- Retrofit22/app/src/main/java/com/example/retrofit2/dto/UserInfo.kt
+++ Retrofit22/app/src/main/java/com/example/retrofit2/dto/UserInfo.kt
@@ -1,10 +1,7 @@
 package com.example.retrofit2.dto
 
-import com.google.gson.annotations.SerializedName
-
 data class UserInfo(
-//    @SerializedName("login")
-    val userId: String,
-    val name: String,
-    val company: String
+    val login: String,
+    val followers: String,
+    val following: String
 )
--- Retrofit22/app/src/main/java/com/example/retrofit2/object/RetrofitInstance.kt
+++ Retrofit22/app/src/main/java/com/example/retrofit2/object/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    const val BASE_URL = "";
+    private const val BASE_URL = "https://api.github.com/";
 
     private val retrofit by lazy {
         Retrofit.Builder()
