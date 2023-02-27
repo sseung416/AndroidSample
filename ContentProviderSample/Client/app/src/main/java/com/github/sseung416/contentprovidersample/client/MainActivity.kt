1e7eebb09b2d19d626aeba03fdfd48cf090cf59b
sseung416 <choiseungyeon416@gmail.com>
2023-02-27 17:54:10 +0900

    Initial Commit

---
+++ ContentProviderSample/Client/app/src/main/java/com/github/sseung416/contentprovidersample/client/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.github.sseung416.contentprovidersample.client
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.sseung416.contentprovidersample.client.ui.theme.ProviderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProviderTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ProviderTheme {
+        Greeting("Android")
+    }
+}
