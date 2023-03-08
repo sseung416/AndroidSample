38bf31360d0537ce2418869a388b4bfae3e21f7b
sseung416 <choiseungyeon416@gmail.com>
2023-03-08 19:55:13 +0900

    Add: ContentProvider 테스트용 UI 화면 추가

--- ContentProviderSample/Client/app/src/main/java/com/github/sseung416/contentprovidersample/client/MainActivity.kt
+++ ContentProviderSample/Client/app/src/main/java/com/github/sseung416/contentprovidersample/client/MainActivity.kt
@@ -3,45 +3,101 @@ package com.github.sseung416.contentprovidersample.client
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.github.sseung416.contentprovidersample.client.local.dto.User
 import com.github.sseung416.contentprovidersample.client.ui.theme.ProviderTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel by viewModels<MainViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel.getAllUsers(contentResolver)
-
         setContent {
             ProviderTheme {
-                val users by viewModel.users.collectAsState()
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    UserList(list = users)
+                    MainScreen()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(
+    viewModel: MainViewModel = viewModel(factory = MainViewModelFactory(LocalContext.current.contentResolver)),
+) {
+    val users by viewModel.users.collectAsState()
+
+    Column {
+        Buttons(
+            viewModel::getAllUsers,
+            viewModel::getTeenagerUsers,
+            viewModel::insertUser,
+            viewModel::updateUser,
+            viewModel::deleteUser
+        )
+        UserList(list = users)
+    }
+}
+
+@Composable
+fun Buttons(
+    getAllUsers: () -> Unit,
+    getTeenagerUsers: () -> Unit,
+    insertUser: (User) -> Unit,
+    updateUser: (User) -> Unit,
+    deleteUser: (id: Int) -> Unit,
+) {
+    var newUserId by remember { mutableStateOf(0) }
+
+    Row {
+        Button(onClick = { getAllUsers.invoke() }) {
+            Text(text = "모든 유저 조회하기")
+        }
+        Button(onClick = { getTeenagerUsers.invoke() }) {
+            Text(text = "10대 유저 조회하기")
+        }
+        Button(onClick = {
+            newUserId++
+            insertUser.invoke(
+                User(
+                    name = "더미$newUserId",
+                    age = newUserId * 3,
+                    favoriteColorId = 1
+                )
+            )
+        }) {
+            Text(text = "유저 추가")
+        }
+        Button(onClick = {
+            updateUser.invoke(
+                User(
+                    id = newUserId,
+                    name = "수정더미$newUserId",
+                    age = newUserId * 3,
+                    favoriteColorId = 1
+                )
+            )
+        }) {
+            Text(text = "최근에 추가된 유저 수정")
+        }
+        Button(onClick = {
+            deleteUser.invoke(newUserId)
+            newUserId--
+        }) {
+            Text(text = "최근에 추가된 유저 삭제")
         }
     }
 }
@@ -55,12 +111,17 @@ fun UserList(list: List<User>) {
 
 @Composable
 fun UserCard(user: User) {
-    Card {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .wrapContentSize()
+    ) {
         Row {
             Text(text = user.name)
-            Text(text = "${user.age}살", color = Color.Gray)
+            Spacer(modifier = Modifier.padding(6.dp))
+            Text(text = "${user.age}살")
         }
     }
 }
 
-val CONTENT_URI = "content://${UserProvider.PROVIDER_URL}/${UserProvider.PATH_USER}".toUri()
+val USER_CONTENT_URI = "content://${UserProvider.PROVIDER_URL}/${UserProvider.PATH_USER}".toUri()
